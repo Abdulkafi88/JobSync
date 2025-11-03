3b212eb7b1236996d8f4c5e47830e9c2abebb88d
Abdulkafi <abdulkafinirig5@gmail.com>
2025-11-03 05:36:39 -0800

    working on the vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/JobSync/",
+  base: "/JobSync",
   server: {
     host: "0.0.0.0",
     port: 8080,
